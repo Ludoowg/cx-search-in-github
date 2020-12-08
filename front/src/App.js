188e74a2acabe2d99ef43ac2e036b2f3b642f3c1
John Doe <ludovic.geran@efrei.net>
2020-12-08 23:37:31 +0100

    lastcommit

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.png';
 import './App.css';
+import { getUser } from './API.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <form>
+    this.state = {
+      github_users: null,
+    }
+  }
 
-        <label>
-          User Name :
-          <input type="text" name="name" />
-        </label>
-         <input type="submit" value="Envoyer" />
-      </form>
-      </header>
-    </div>
-  );
+  async loadUser() {
+    try {
+      const USERFromAPI = await getUser();
+      this.setState({ github_users: USERFromAPI })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          <div className="App">
+            <header className="App-header">
+            <p>{github_users.login}</p>
+          </header>
+          </div>
+          <form>
+            <label>
+              User Name :
+              <input type="text" name="name" />
+            </label>
+             <input type="submit" value="Envoyer" />
+          </form>
+          </header>
+        </div>
+      );
+  }
 }
+
+
+
 
 export default App;
